88680d7221a1b8d24604b31294c531e66c226a08
Nir <nirt04@gmail.com>
2021-01-09 13:21:23 +0200

    instructions

--- services/util.js
+++ services/util.js
@@ -20,8 +20,8 @@ const getPremissionOfUser = (userPermissons, tree) => {
   // 1. is it match? Great lets continue.
   // 2. is it leaf? OK GO TO userPremissionsExpanded!
   // 3. is it root? OK EXCUTE getAllLeafsOfNode(<NODE>) and store all of them in userPremissionsExpanded but before:...
-  //      a. since we expanded all the node and got all his leafs there is a big chance that on of the next user premissions that we attend to loop over, is already added to userPremissionsExpanded.
-  //      so before we store all of them in userPremissionsExpanded we delete all of them from userPermissons so we wont have to loop over things we got.
+  //      a. since we expanded all the node and got all his leafs there is a big chance that on of the next user premissions that we attend to loop over, is already added to the userPremissionsExpanded.
+  //      so before we store all of them in userPremissionsExpanded we delete all of them from userPermissons so we wont have to loop over things we already have.
 
   return { userPermissons, tree };
 };
