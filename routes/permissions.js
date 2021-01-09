728701871fdb0c4fe8b2eecdf9da6de1ad79994a
Nir <nirt04@gmail.com>
2021-01-09 08:41:31 +0200

    permissions routing

--- routes/permissions.js
+++ routes/permissions.js
@@ -3,7 +3,8 @@ const util = require('../services/util');
 function permissions(app) {
   app.get('/get_all_leafs', async (req, res) => {
     util.getAllLeafs({ a: '1' }, ['hello']);
-    res.send('hello ');
+    const worksheet = util.getWorkSheetsFromFile('../data/permissions.xlsx');
+    res.send('hello permissions worksheet:', worksheet);
   });
 }
 
--- services/util.js
+++ services/util.js
@@ -1,6 +1,17 @@
+// import xlsx from 'node-xlsx';
+const xlsx = require('node-xlsx').default;
+
+// Parse a buffer
+// const workSheetsFromBuffer = xlsx.parse(fs.readFileSync(`${__dirname}/myFile.xlsx`));
+// Parse a file
+const getWorkSheetsFromFile = (path) => {
+  const workSheetsFromFile = xlsx.parse(path);
+  return workSheetsFromFile;
+};
 const getAllLeafs = (tree, kinds) => {
   console.log('hello getAllLeafs');
 };
 module.exports = {
   getAllLeafs,
+  getWorkSheetsFromFile,
 };
