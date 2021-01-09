27b8bbde74eedd6df5d6d2532715223fdf617899
Nir <nirt04@gmail.com>
2021-01-09 13:11:44 +0200

    instructions

--- routes/permissions.js
+++ routes/permissions.js
@@ -1,11 +1,20 @@
 const util = require('../services/util');
 
+const permissions_worksheet = util.getWorkSheetsFromFile(
+  `${__dirname}/../data/xlsx/permissions.xlsx`
+);
+const users_worksheet = util.getWorkSheetsFromFile(
+  `${__dirname}/../data/xlsx/user_permissions.xlsx`
+);
+
 function permissions(app) {
-  app.get('/get_all_leafs', async (req, res) => {
+  app.get('/get-premission-of-user', async (req, res) => {
     try {
-      util.getAllLeafs({ a: '1' }, ['hello']);
-      const worksheet = util.getWorkSheetsFromFile(`${__dirname}/../data/xlsx/permissions.xlsx`);
-      res.status('200').send(worksheet);
+      const USER_PREMISSIONS = util.getPremissionOfUser(
+        users_worksheet.userPermissions,
+        permissions_worksheet.authTree
+      );
+      res.status('200').send(USER_PREMISSIONS);
     } catch (error) {
       console.log('error', error);
     }
--- services/util.js
+++ services/util.js
@@ -9,10 +9,30 @@ const getWorkSheetsFromFile = (path) => {
   return worksheets;
 };
 
+const getPremissionOfUser = (userPermissons, tree) => {
+  let userPremissionsExpanded = {
+
+  };
+  const getAllLeafsOfNode = (node) => {
+    // TODO RUN OVER BINARY TREE AND GET ALL LEAFS
+  };
+
+  // TODO Loop over tree and for each node loop the userPermissons and ask:
+
+  // a. is it match? Great lets continue.
+  // b. is it leaf? OK GO TO userPremissionsExpanded! 
+  // c. is it root? OK EXCUTE getAllLeafsOfNode(<NODE>) and store all of them in userPremissionsExpanded but before:... 
+  // d. since we expanded all the node and got all his leafs there is a big chance that on of the next user premissions that we attend to loop over, is already added to userPremissionsExpanded. 
+  //    so before we store all of them in userPremissionsExpanded we delete all of them from userPermissons so we wont have to loop over things we got.
+
+  return { userPermissons, tree };
+};
+
 const getAllLeafs = (tree, kinds) => {
   // TODO
 };
 module.exports = {
   getAllLeafs,
   getWorkSheetsFromFile,
+  getPremissionOfUser,
 };
